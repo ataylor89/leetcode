d15e0056b21ec1f9c14b63921b5a43564ee3c78d
Andrew <rolyata47@gmail.com>
2020-01-11 16:35:08 -0600

    Committing problem 217

---
+++ problem0217.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        map<int,bool> m;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            if (m.find(nums[i]) != m.end())
+                return true;
+            
+            m[nums[i]] = true;
+        }
+        
+        return false;
+    }
+};
